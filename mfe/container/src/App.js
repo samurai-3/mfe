179402664ba82e9fdcd09d98890ae341eb669de8
samurai-3 <64775120+samurai-3@users.noreply.github.com>
2025-03-20 12:24:39 +0530

    Update App.js

--- mfe/container/src/App.js
+++ mfe/container/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 const App = () => {
-    const [count, setCount] = useState(0);
+     const [count, setCount] = useState(0);
 
     // Incorrectly using useEffect (infinite loop risk)
-    useEffect(() => {
-        setCount(count + 1);
+    useEffect(() =>  {
+         setCount(count + 1)
     });
 
     // Incorrect event handler binding in render
-    const handleClick = () => {
-        alert('Count is: ' + count);
+    const  handleClick = () => {
+         alert('Count is: ' + count);
     };
     return (
         <div>
