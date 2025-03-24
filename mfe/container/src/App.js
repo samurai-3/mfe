d271f4f300a0f10501ab6f30a2e0113b6c0c7437
samurai-3 <64775120+samurai-3@users.noreply.github.com>
2025-03-24 12:00:37 +0530

    Update App.js

--- mfe/container/src/App.js
+++ mfe/container/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 const App = () => {
+    const [count, setCount] = useState;
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, []); 
+
+  const increment = () => {
+    setCount(count + 1);
+  };
     return (
         <div>
             <MarketingApp />
